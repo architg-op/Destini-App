a3f3212cde73c4df0ee0488c21685b18fd978986
Archit Garg <archit.g1998@gmail.com>
2019-02-18 22:43:50 +0530

    Assets added

---
+++ Destini/ViewController.swift
@@ -0,0 +1,155 @@
+//
+//  ViewController.swift
+//  Destini
+//
+//  Created by Archit Garg on 30/12/2018.
+//  Copyright (c) 2018 Archit tech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    var storyNumber : Int = 1
+    // Our strings
+    let story1 = "Your car has blown a tire on a winding road in the middle of nowhere with no cell phone reception. You decide to hitchhike. A rusty pickup truck rumbles to a stop next to you. A man with a wide brimmed hat with soulless eyes opens the passenger door for you and asks: \"Need a ride, boy?\"."
+    let answer1a = "I\'ll hop in. Thanks for the help!"
+    let answer1b = "Better ask him if he\'s a murderer first."
+    
+    
+    let story2 = "He nods slowly, unphased by the question."
+    let answer2a = "At least he\'s honest. I\'ll climb in."
+    let answer2b = "Wait, I know how to change a tire."
+    
+    let story3 = "As you begin to drive, the stranger starts talking about his relationship with his mother. He gets angrier and angrier by the minute. He asks you to open the glovebox. Inside you find a bloody knife, two severed fingers, and a cassette tape of Elton John. He reaches for the glove box."
+    let answer3a = "I love Elton John! Hand him the cassette tape."
+    let answer3b = "It\'s him or me! You take the knife and stab him."
+    
+    let story4 = "What? Such a cop out! Did you know traffic accidents are the second leading cause of accidental death for most adult age groups?"
+    let story5 = "As you smash through the guardrail and careen towards the jagged rocks below you reflect on the dubious wisdom of stabbing someone while they are driving a car you are in."
+    let story6 = "You bond with the murderer while crooning verses of \"Can you feel the love tonight\". He drops you off at the next town. Before you go he asks you if you know any good places to dump bodies. You reply: \"Try the pier.\""
+    
+    
+    // UI Elements linked to the storyboard
+    @IBOutlet weak var topButton: UIButton!         // Has TAG = 1
+    @IBOutlet weak var bottomButton: UIButton!      // Has TAG = 2
+    @IBOutlet weak var storyTextView: UILabel!
+    @IBOutlet weak var restartButton: UIButton!
+    
+    // TODO Step 5: Initialise instance variables here
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        restartButton.isHidden = true
+        storyTextView.text = story1
+        topButton.setTitle(answer1a, for: .normal)
+        bottomButton.setTitle(answer1b, for: .normal)
+        
+        // TODO Step 3: Set the text for the storyTextView, topButton, bottomButton, and to T1_Story, T1_Ans1, and T1_Ans2
+        
+    }
+
+    
+    // User presses one of the buttons
+    @IBAction func restartButtonPressed(_ sender: UIButton) {
+        restartButton.isHidden = true
+        topButton.isHidden = false
+        bottomButton.isHidden = false
+        storyNumber = 1
+        storyTextView.text = story1
+        topButton.setTitle(answer1a, for: .normal)
+        bottomButton.setTitle(answer1b, for: .normal)
+    }
+    @IBAction func buttonPressed(_ sender: UIButton) {
+    
+        // TODO Step 4: Write an IF-Statement to update the views
+                
+        // TODO Step 6: Modify the IF-Statement to complete the story
+        
+        if ((storyNumber == 1 || storyNumber == 2) && sender.tag == 1) {
+            storyNumber = 3
+            storyTextView.text = story3
+            topButton.setTitle(answer3a, for: .normal)
+            bottomButton.setTitle(answer3b, for: .normal)
+        }
+        else if (storyNumber == 1 && sender.tag == 2) {
+            storyNumber = 2
+            storyTextView.text = story2
+            topButton.setTitle(answer2a, for: .normal)
+            bottomButton.setTitle(answer2b, for: .normal)
+        }
+        else if (storyNumber == 3 && sender.tag == 1) {
+            storyNumber = 6
+            storyTextView.text = story6
+            topButton.isHidden = true
+            bottomButton.isHidden = true
+        }
+        else if (storyNumber == 3 && sender.tag == 2) {
+            storyNumber = 5
+            storyTextView.text = story5
+            topButton.isHidden = true
+            bottomButton.isHidden = true
+        }
+        else if (storyNumber == 2 && sender.tag == 2) {
+            storyNumber = 4
+            storyTextView.text = story4
+            topButton.isHidden = true
+            bottomButton.isHidden = true
+        }
+        if (storyNumber == 4 || storyNumber == 5 || storyNumber == 6) {
+            restartButton.isHidden = false
+        }
+        /*if sender.tag == 1 {
+            if storyNumber == 1 {
+                storyNumber = 3
+                storyTextView.text = story3
+                topButton.setTitle(answer3a, for: .normal)
+                bottomButton.setTitle(answer3b, for: .normal)
+            }
+            else if storyNumber == 3 {
+                storyNumber = 6
+                storyTextView.text = story6
+                topButton.isHidden = true
+                bottomButton.isHidden = true
+            }
+            else if storyNumber == 2 {
+                storyNumber = 3
+                storyTextView.text = story3
+                topButton.setTitle(answer3a, for: .normal)
+                bottomButton.setTitle(answer3b, for: .normal)
+            }
+            
+            
+        }
+        else if sender.tag == 2 {
+            if storyNumber == 1 {
+                storyNumber = 2
+                storyTextView.text = story2
+                topButton.setTitle(answer2a, for: .normal)
+                bottomButton.setTitle(answer2b, for: .normal)
+            }
+            else if storyNumber == 3 {
+                storyNumber = 5
+                storyTextView.text = story5
+                topButton.isHidden = true
+                bottomButton.isHidden = true
+            }
+            else if storyNumber == 2 {
+                storyNumber = 4
+                storyTextView.text = story4
+                topButton.isHidden = true
+                bottomButton.isHidden = true
+            }
+            
+        }*/
+    
+    }
+    
+
+
+
+}
+
